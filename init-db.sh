849163afb545c807309fe01297e46e3a3b117e0f
Emanuel Rodriguez <erodriguez@flowwest.com>
2022-06-16 11:35:22 -0700

    renames files, deletes old script and adds init datbase script

--- init-db.sh
+++ init-db.sh
@@ -2,10 +2,10 @@
 source env.sh 
 
 # drop tables
-psql -h $DB_HOST -d $DB_NAME -U $DB_USERNAME -p $DB_PORT -f tables/drop_tables.sql 
+psql -f tables/drop-tables.sql 
 
 # create tables
-psql -h $DB_HOST -d $DB_NAME -U $DB_USERNAME -p $DB_PORT -f  tables/create-tables.sql
+psql -f tables/create-tables.sql
 
 # seed lookups
 # psql -h $HOST -d $DB -p $PASSWORD -U $USERNAME -p $PORT -f  seedData/seed_data.sql
