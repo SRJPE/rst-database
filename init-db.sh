bb400127f0acc5a1353bba1620c84f2cd3499d10
Emanuel Rodriguez <erodriguez@flowwest.com>
2022-06-16 11:41:13 -0700

    change line endings to LF

--- init-db.sh
+++ init-db.sh
@@ -1,11 +1,11 @@
 # load env vars for db creds
-source env.sh 
+source env.sh
 
 # drop tables
-psql -f tables/drop-tables.sql 
+psql -f tables/drop-tables.sql
 
 # create tables
 psql -f tables/create-tables.sql
 
 # seed lookups
-# psql -h $HOST -d $DB -p $PASSWORD -U $USERNAME -p $PORT -f  seedData/seed_data.sql
+psql -f  seedData/seed_data.sql
