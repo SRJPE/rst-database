7c0ecef7d173e2ebb69875df813f1eaa22b12c9b
Jordan Hoang <jordan.n.hoang@gmail.com>
2022-11-15 13:32:45 -0600

    refactors initial seed script to check for node env to run specific seed file

--- knex/seeds/01_initial_seed.js
+++ knex/seeds/01_initial_seed.js
@@ -7,10 +7,22 @@ const fs = require('fs')
 const path = require('path')
 
 exports.seed = async function (knex) {
-  return knex.raw(
-    fs.readFileSync(
-      path.join(__dirname, '../../seed_data/seed_data.sql'),
-      'utf8'
+  if (process.NODE_ENV === 'development') {
+    return knex.raw(
+      fs.readFileSync(
+        path.join(__dirname, '../../seed_data/seed_data.sql'),
+        'utf8'
+      )
     )
-  )
+  } else if (
+    process.NODE_ENV === 'staging' ||
+    process.NODE_ENV === 'production'
+  ) {
+    return knex.raw(
+      fs.readFileSync(
+        path.join(__dirname, '../../seed_data/seed_azure.sql'),
+        'utf8'
+      )
+    )
+  }
 }
