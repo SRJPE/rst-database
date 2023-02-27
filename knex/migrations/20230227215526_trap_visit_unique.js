68c1216f6641cebbd78f9b2d52c5d9ee41f8fc1a
Jordan Hoang <jordan.n.hoang@gmail.com>
2023-02-27 17:08:19 -0600

    adds temp fix because existing staging db has constraint violating data

--- knex/migrations/20230227215526_trap_visit_unique.js
+++ knex/migrations/20230227215526_trap_visit_unique.js
@@ -3,8 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
+  // return knex.raw(`
+  //   ALTER TABLE trap_visit ADD CONSTRAINT unique_trap_visit UNIQUE (program_id, trap_location_id, trap_visit_time_start) NOVALIDATE;
+  //   `)
+
   return knex.raw(`
-    ALTER TABLE trap_visit ADD CONSTRAINT unique_trap_visit UNIQUE (program_id, trap_location_id, trap_visit_time_start);
+    CREATE UNIQUE INDEX unique_trap_visit
+    ON trap_visit (program_id, trap_location_id, trap_visit_time_start)
+    WHERE created_at >= '2023-02-28'::timestamp;
     `)
 }
 
@@ -13,7 +19,11 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
+  // return knex.raw(`
+  //   ALTER TABLE trap_visit DROP CONSTRAINT unique_trap_visit;
+  //   `)
+
   return knex.raw(`
-    ALTER TABLE trap_visit DROP CONSTRAINT unique_trap_visit;
+    DROP INDEX IF EXISTS unique_trap_visit;
     `)
 }
