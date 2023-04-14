ed7758476fd83d6b203db8e42a08e88a9ea92188
Hunter H <hunter.h.dev@gmail.com>
2023-04-14 11:59:16 -0700

    Update program_personnel_team seeding file

--- knex/seeds/04_program_personnel_team.js
+++ knex/seeds/04_program_personnel_team.js
@@ -3,29 +3,36 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-
-  const allSeededPersonnel = await knex('personnel').select('id')
-
-  const rowsToInsert = allSeededPersonnel.reduce(
-    (previousArray, currentPersonnel) => {
-      updatedArray = previousArray.concat([
-        {
-          personnelId: currentPersonnel.id,
-          programId: 1,
-        },
-        {
-          personnelId: currentPersonnel.id,
-          programId: 2,
-        },
-      ])
-
-      return updatedArray
-    },
-    []
-  )
-
-  await knex('program_personnel_team').del()
-  // seed program_personnel_team
-  await knex('program_personnel_team').insert(rowsToInsert)
+  try {
+    // Deletes ALL existing entries
+    await knex('program_personnel_team').del()
+    // seed program_personnel_team
+    await knex('program_personnel_team').insert([
+      { personnel_id: 1, program_id: 1 },
+      { personnel_id: 1, program_id: 2 },
+      { personnel_id: 2, program_id: 1 },
+      { personnel_id: 2, program_id: 2 },
+      { personnel_id: 3, program_id: 1 },
+      { personnel_id: 3, program_id: 2 },
+      { personnel_id: 4, program_id: 1 },
+      { personnel_id: 4, program_id: 2 },
+      { personnel_id: 5, program_id: 1 },
+      { personnel_id: 5, program_id: 2 },
+      { personnel_id: 6, program_id: 1 },
+      { personnel_id: 6, program_id: 2 },
+      { personnel_id: 7, program_id: 1 },
+      { personnel_id: 7, program_id: 2 },
+      { personnel_id: 8, program_id: 1 },
+      { personnel_id: 8, program_id: 2 },
+      { personnel_id: 9, program_id: 3 },
+      { personnel_id: 10, program_id: 4 },
+      { personnel_id: 11, program_id: 5 },
+      { personnel_id: 12, program_id: 5 },
+      { personnel_id: 13, program_id: 5 },
+      { personnel_id: 14, program_id: 5 },
+    ])
+    
+  } catch (error) {
+    throw error
+  }
 }
