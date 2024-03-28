32e07bf3004e4739e54a0e4307d1e5362cfb580d
Jordan Hoang <jordan.n.hoang@gmail.com>
2024-03-28 14:13:24 -0500

    adds migration for mark type drop

---
+++ knex/migrations/20240328191048_drop_catch_raw_mark_type.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(`
+    ALTER TABLE catch_raw DROP COLUMN mark_type;
+  `)
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(`
+    ALTER TABLE catch_raw ADD mark_type INTEGER references mark_type,;
+    `)
+}
