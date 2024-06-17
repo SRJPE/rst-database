80f1a3c0700e276d8f8947436057182dc593ce08
Jordan Hoang <jordan.n.hoang@gmail.com>
2024-06-17 11:41:58 -0500

    removes now ignored files

--- knex/seeds/09_fish_condition.js
+++ knex/seeds/09_fish_condition.js
@@ -16,24 +16,4 @@ exports.seed = async function (knex) {
     { definition: 'fungus' },
     { definition: 'none' },
   ])
-  // Deletes ALL existing entries
-  await knex('take_and_mortality').del()
-  await knex('take_and_mortality').insert([
-    {
-      permitInfoId: 1,
-      species: '161980',
-      listingUnit: 1,
-      fishLifeStage: 9,
-      allowedExpectedTake: 1950,
-      allowedMortalityCount: 10,
-    },
-    {
-      permitInfoId: 2,
-      species: '161980',
-      listingUnit: 1,
-      fishLifeStage: 9,
-      allowedExpectedTake: 1950,
-      allowedMortalityCount: 10,
-    },
-  ])
 }
