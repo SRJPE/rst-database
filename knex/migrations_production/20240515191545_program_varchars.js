643fa585473b48128a65d3c6edc6e64fbaf1411b
Jordan Hoang <jordan.n.hoang@gmail.com>
2024-09-03 11:53:47 -0500

    changes varchar contraint for program and strema

--- knex/migrations_production/20240515191545_program_varchars.js
+++ knex/migrations_production/20240515191545_program_varchars.js
@@ -15,7 +15,7 @@ exports.up = function (knex) {
  */
 exports.down = function (knex) {
   return knex.raw(`
-    ALTER TABLE program ALTER COLUMN program_name TYPE VARCHAR(25);
-    ALTER TABLE program ALTER COLUMN stream_name TYPE VARCHAR(25);
+    ALTER TABLE program ALTER COLUMN program_name TYPE VARCHAR(100);
+    ALTER TABLE program ALTER COLUMN stream_name TYPE VARCHAR(100);
   `)
 }
