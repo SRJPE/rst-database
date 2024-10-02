ecb208446f8be44cfdca02af4e7230f01ac73dbf
Jordan Hoang <jordan.n.hoang@gmail.com>
2024-10-02 09:08:59 -0500

    adds new migrations

---
+++ knex/migrations/20240919153044_drop.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(`
+    ALTER TABLE catch_raw DROP COLUMN release_id;
+    `)
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(`
+    ALTER TABLE catch_raw ADD COLUMN release_id INTEGER REFERENCES release;
+    `)
+}
