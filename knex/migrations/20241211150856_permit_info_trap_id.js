eb3827cfd5c0eb530c01b0be4c6f6d37ce922d69
Jordan Hoang <jordan.n.hoang@gmail.com>
2024-12-11 12:13:29 -0600

    adds permit info values

---
+++ knex/migrations/20241211150856_permit_info_trap_id.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(`
+    ALTER TABLE permit_info ADD COLUMN trap_locations_id INTEGER REFERENCES trap_locations;
+    `)
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(`
+    ALTER TABLE permit_info DROP COLUMN trap_locations_id;
+    `)
+}
