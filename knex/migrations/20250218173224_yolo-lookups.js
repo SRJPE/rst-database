64fef33acd1a0a6cec12f67e40c8fb3218e838af
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-03-10 18:28:55 -0500

    adds yolo fields

--- knex/migrations/20250218173224_yolo-lookups.js
+++ knex/migrations/20250218173224_yolo-lookups.js
@@ -59,6 +59,14 @@ exports.up = function (knex) {
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
+
+    CREATE TABLE IF NOT EXISTS ysi_num (
+      id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      code varchar(10) NOT NULL,
+      description text NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
   `)
 }
 
---
+++ knex/migrations_production/20250221163613_additional-trap-fields.js
@@ -0,0 +1,50 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(`
+    ALTER TABLE trap_visit ADD COLUMN vegetation_code INTEGER NULL REFERENCES vegetation_code;
+    ALTER TABLE trap_visit ADD COLUMN condition_code INTEGER NULL REFERENCES condition_code;
+    ALTER TABLE trap_visit ADD COLUMN gear_status INTEGER NULL REFERENCES gear_status;
+
+
+    CREATE TYPE field_type_enum AS ENUM ('input', 'select', 'multi-select', 'radio', 'checkbox', 'textarea');
+    CREATE TYPE form_section_enum AS ENUM ('Visit Setup', 'Trap Operations', 'Fish Processing', 'Fish Input', 'Trap Post-Processing');
+
+
+    CREATE TABLE IF NOT EXISTS form_field (
+      id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      field_name VARCHAR(100) NOT NULL,
+      display_name VARCHAR(100) NOT NULL,
+      unit_id INTEGER REFERENCES unit NULL,
+      field_type field_type_enum NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS program_fields (
+      id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      program_id INTEGER REFERENCES program,
+      form_field_id INTEGER REFERENCES form_field,
+      required BOOLEAN NOT NULL,
+      form_section form_section_enum NOT NULL,
+      order_index INTEGER
+    );
+    `)
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(`
+    ALTER TABLE trap_visit DROP COLUMN vegetation_code;
+    ALTER TABLE trap_visit DROP COLUMN condition_code;
+    ALTER TABLE trap_visit DROP COLUMN gear_status;
+
+    DROP TABLE IF EXISTS form_field CASCADE;
+    DROP TABLE IF EXISTS program_fields CASCADE;
+    DROP TYPE IF EXISTS field_type_enum;
+    DROP TYPE IF EXISTS form_section_enum;
+    `)
+}
