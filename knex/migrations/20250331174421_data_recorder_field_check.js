c491198be276f2a9004363e7b4fb268bfc61ab4b
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-04-04 12:28:03 -0500

    updayes migrations and seeds for sample equipment type

---
+++ knex/migrations/20250331174421_data_recorder_field_check.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(`
+    ALTER TABLE trap_visit_crew ADD COLUMN data_recorder BOOLEAN NULL;
+    ALTER TABLE trap_visit_crew ADD COLUMN field_check BOOLEAN NULL;
+
+    ALTER TABLE trap_locations ADD COLUMN equipment_id INTEGER references equipment;
+    `)
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(`
+    ALTER TABLE trap_visit_crew DROP COLUMN data_recorder;
+    ALTER TABLE trap_visit_crew DROP COLUMN field_check;
+    ALTER TABLE trap_locations DROP COLUMN equipment_id;
+    `)
+}
