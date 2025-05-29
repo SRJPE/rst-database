ee4f4bf8c615a5141b1eb506a56e3e740079e64a
Jordan Hoang <jordan.n.hoang@gmail.com>
2025-05-29 13:06:53 -0500

    adds eggs migration

---
+++ knex/migrations_production/20250529173920_fish_eggs.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(`
+    ALTER TABLE catch_raw ADD COLUMN eggs BOOLEAN NULL;
+    `)
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(`
+    ALTER TABLE catch_raw DROP COLUMN eggs;
+    `)
+}
